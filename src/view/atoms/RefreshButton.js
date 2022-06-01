9e724f6d1b994fcf2ff7443add51837b5f1f30bf
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-01 06:27:43 +0530

    Added external onClick

--- src/view/atoms/RefreshButton.js
+++ src/view/atoms/RefreshButton.js
@@ -1,13 +1,10 @@
 import IconButton from "@mui/material/IconButton";
 import RefreshIcon from "@mui/icons-material/Refresh";
 
-export default function RefreshButton() {
+export default function RefreshButton({ onClick }) {
   return (
     <IconButton
-      onClick={function () {
-        localStorage.clear();
-        window.location.reload();
-      }}
+      onClick={onClick}
       style={{ position: "fixed", top: "2%", right: "5%" }}
     >
       <RefreshIcon />
