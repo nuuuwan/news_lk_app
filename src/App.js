1758a074a6a04627cf6ad855ef7b26052365c2d1
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-01 11:17:44 +0530

    Removed VersioNWidget

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Box from "@mui/material/Box";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import SriLankaColors from "./view/_constants/SriLankaColors";
-import VersionWidget from "./view/molecules/VersionWidget";
 import HomePage from "./view/pages/HomePage.js";
 
 const THEME = createTheme({
@@ -34,7 +33,6 @@ export default class App extends Component {
       <ThemeProvider theme={THEME}>
         <Box sx={{ maxWidth: "90%", width: 500, margin: "auto" }}>
           <HomePage />
-          <VersionWidget />
         </Box>
       </ThemeProvider>
     );
