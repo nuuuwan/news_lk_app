61ca620d61cec9b3baef5718bdaedd836010e026
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-01 11:39:59 +0530

    Render ArticleSummary

--- src/view/molecules/ArticleViewMolecule.js
+++ src/view/molecules/ArticleViewMolecule.js
@@ -1,38 +1,56 @@
 import Box from "@mui/material/Box";
+import CircularProgress from "@mui/material/CircularProgress";
 import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
 import { useTheme } from "@mui/material/styles";
 
+import Condition from "../../view/atoms/Condition";
 import DotSeparator from "../../view/molecules/DotSeparator";
 import LimitWords from "../../view/molecules/LimitWords";
 
-export default function ArticleViewMolecule({ article }) {
+export default function ArticleViewMolecule({ articleSummary, article }) {
   const theme = useTheme();
   const colorTitle = theme.palette.primary.main;
   const colorDate = theme.palette.secondary.main;
   const colorNewspaper = theme.palette.success.main;
 
+  const isArticleNotNull = article !== null;
+
   return (
     <Box>
-      <Link href={article.url} target="_blank" sx={{ textDecoration: "none" }}>
+      <Link
+        href={articleSummary.url}
+        target="_blank"
+        sx={{ textDecoration: "none" }}
+      >
         <Typography
           variant="caption"
           component="div"
           sx={{ color: colorNewspaper }}
         >
-          {article.urlShort}
+          {articleSummary.urlShort}
         </Typography>
       </Link>
-      <DotSeparator sx={{ color: colorDate }}>
-        <Typography variant="caption">
-          {article.readingTimeMinutes + " minute read"}
-        </Typography>
-        <Typography variant="caption">{article.timeStrHumanized}</Typography>
-      </DotSeparator>
+
       <Typography variant="h6" sx={{ color: colorTitle }}>
-        {article.title}
+        {articleSummary.title}
       </Typography>
-      <LimitWords lines={article.bodyLines} wordLimit={50} />
+
+      <Condition condition={isArticleNotNull}>
+        <DotSeparator sx={{ color: colorDate }}>
+          <Typography variant="caption">
+            {article?.readingTimeMinutes + " minute read"}
+          </Typography>
+          <Typography variant="caption">
+            {articleSummary.timeStrHumanized}
+          </Typography>
+        </DotSeparator>
+        <LimitWords lines={article?.bodyLines} wordLimit={50} />
+      </Condition>
+
+      <Condition condition={!isArticleNotNull}>
+        <CircularProgress />
+      </Condition>
     </Box>
   );
 }
