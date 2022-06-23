d02059218fed1ad23a3dedc4eb78e6cd0c7d5e0d
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-23 11:20:53 +0530

    i18n

--- src/nonview/base/DICTIONARY.js
+++ src/nonview/base/DICTIONARY.js
@@ -1,7 +1,30 @@
 const DICTIONARY = {
-  Test: {
-    si: "\u0db4\u0dbb\u0dd3\u0d9a\u0dca\u0dc2\u0dab\u0dba",
-    ta: "\u0b9a\u0bcb\u0ba4\u0ba9\u0bc8",
+  "Test": {
+    "si": "\u0db4\u0dbb\u0dd3\u0d9a\u0dca\u0dc2\u0dab\u0dba",
+    "ta": "\u0b9a\u0bcb\u0ba4\u0ba9\u0bc8"
   },
-};
-export default DICTIONARY;
+  "ago": {
+    "si": "\u0db4\u0dd9\u0dbb",
+    "ta": "\u0bae\u0bc1\u0ba9\u0bcd\u0baa\u0bc1"
+  },
+  "days": {
+    "si": "\u0daf\u0dd2\u0db1",
+    "ta": "\u0ba8\u0bbe\u0b9f\u0bcd\u0b95\u0bb3\u0bbf\u0bb2\u0bcd"
+  },
+  "hours": {
+    "si": "\u0db4\u0dd0\u0dba",
+    "ta": "\u0bae\u0ba3\u0bbf"
+  },
+  "minutes": {
+    "si": "\u0db8\u0dd2\u0db1\u0dd2\u0dad\u0dca\u0dad\u0dd4",
+    "ta": "\u0ba8\u0bbf\u0bae\u0bbf\u0b9f\u0b99\u0bcd\u0b95\u0bb3\u0bcd"
+  },
+  "seconds": {
+    "si": "\u0dad\u0dad\u0dca\u0db4\u0dbb",
+    "ta": "\u0bb5\u0bbf\u0ba9\u0bbe\u0b9f\u0bbf\u0b95\u0bb3\u0bcd"
+  },
+  "weeks": {
+    "si": "\u0dc3\u0dad\u0dd2",
+    "ta": "\u0bb5\u0bbe\u0bb0\u0b99\u0bcd\u0b95\u0bb3\u0bcd"
+  }
+}; export default DICTIONARY;
