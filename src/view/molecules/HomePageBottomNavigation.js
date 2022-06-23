c87b3425918b700c90846b226a753532ed005472
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-23 11:02:26 +0530

    Removed Refresh Label

--- src/view/molecules/HomePageBottomNavigation.js
+++ src/view/molecules/HomePageBottomNavigation.js
@@ -3,17 +3,12 @@ import React from "react";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import Paper from "@mui/material/Paper";
-import Typography from "@mui/material/Typography";
 import RefreshIcon from "@mui/icons-material/Refresh";
-
-import TimeXFuture from "../../nonview/base/TimeXFuture";
 
 export default function HomePageBottomNavigation({
   timeLatestRefresh,
   onClickRefresh,
 }) {
-  const timeLatestRefreshStr =
-    "Last Refreshed " + TimeXFuture.localeString(timeLatestRefresh);
   return (
     <Paper
       sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
@@ -21,11 +16,6 @@ export default function HomePageBottomNavigation({
     >
       <BottomNavigation showLabels>
         <BottomNavigationAction
-          label={
-            <Typography sx={{ fontSize: "xx-small" }}>
-              {timeLatestRefreshStr}
-            </Typography>
-          }
           icon={<RefreshIcon />}
           onClick={onClickRefresh}
         />
