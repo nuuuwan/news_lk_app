f6c9ed3847647d3b56e3c3595e0a2e0773be159b
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-24 09:05:44 +0530

    Merged Translated Article into Article

--- src/nonview/core/Article.js
+++ src/nonview/core/Article.js
@@ -3,15 +3,42 @@ import { JSONWWW } from "../../nonview/base/WWW";
 import { URL_DATA } from "../../nonview/constants/Data";
 import ArticleSummary from "../../nonview/core/ArticleSummary";
 
-const URL_ARTICLES = URL_DATA + "/articles";
+const URL_ARTICLES = URL_DATA + "/articles.translated";
 const MAX_WORDS_BODY_LINES_LIMITED = 100;
 const READING_SPEED_WPM = 200;
 
 export default class Article extends ArticleSummary {
-  constructor(newspaperID, url, timeUT, title, bodyLines, originalLang) {
+  constructor(newspaperID, url, timeUT, title, bodyLines, originalLang, translate) {
     super(newspaperID, url, timeUT, title, null, null);
     this.bodyLines = bodyLines;
     this.originalLang = originalLang;
+    this.translate = translate;
+  }
+
+  static fromDict(d) {
+    let translate;
+    if (d["translate"]) {
+      translate = Object.entries(d["translate"]).reduce(function (
+        translate,
+        [lang, values]
+      ) {
+        translate[lang] = {
+          title: values["title"],
+          bodyLines: values["body_lines"],
+        };
+        return translate;
+      },
+      {});
+    }
+    return new Article(
+      d["newspaper_id"],
+      d["url"],
+      parseInt(d["time_ut"]),
+      d["title"],
+      d["body_lines"],
+      d["original_lang"],
+      translate
+    );
   }
 
   get bodyLinesLimited() {
@@ -45,16 +72,7 @@ export default class Article extends ArticleSummary {
     return Math.ceil(this.wordCount / READING_SPEED_WPM);
   }
 
-  static fromDict(d) {
-    return new Article(
-      d["newspaper_id"],
-      d["url"],
-      parseInt(d["time_ut"]),
-      d["title"],
-      d["body_lines"],
-      d["original_lang"]
-    );
-  }
+
 
   static async loadRawArticle(fileName) {
     const urlArticle = [URL_ARTICLES, fileName].join("/");
