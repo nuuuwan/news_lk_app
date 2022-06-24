b3cd54c7ba03fb56d5237da9ddfc87734fd7942e
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-24 18:28:26 +0530

    Load only 32 articles

--- src/view/pages/HomePage.js
+++ src/view/pages/HomePage.js
@@ -20,7 +20,7 @@ const STYLE = {
   marginBottom: 5,
 };
 
-const MAX_ARTICLES_TO_DISPLAY = 100;
+const MAX_ARTICLES_TO_DISPLAY = 32;
 
 export default class HomePage extends Component {
   constructor(props) {
