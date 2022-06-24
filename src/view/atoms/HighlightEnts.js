d6a601f147f2de022eb09ebf4dd74c57c29abad5
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-24 14:39:47 +0530

    Added HighlightEnts

---
+++ src/view/atoms/HighlightEnts.js
@@ -0,0 +1,89 @@
+function getAnnotatedPairs(text, ents) {
+  let i = 0;
+  let parts = [];
+  while (i < text.length) {
+    let minJ = undefined;
+    let minEnt = undefined;
+    for (let ent of ents) {
+      const needle = ent.text;
+      const j = text.indexOf(needle, i);
+      if (j !== -1) {
+        if (minJ === undefined || j < minJ) {
+          minJ = j;
+          minEnt = ent;
+        }
+      }
+    }
+
+    if (minJ !== undefined) {
+      parts.push([text.substring(i, minJ), false]);
+      parts.push([minEnt.text, minEnt.label]);
+      i = minJ + minEnt.text.length;
+    } else {
+      parts.push([text.substring(i), false]);
+      break;
+    }
+  }
+  return parts;
+}
+
+const STYLE_REGULAR = {};
+
+const STYLE_ENT_THING = {
+  color: "blue",
+};
+
+const STYLE_ENT_NUMBER = {
+  color: "purple",
+};
+
+export default function HighlightEnts({ text, ents }) {
+  if (!ents) {
+    return text;
+  }
+
+  const annotatedPairs = getAnnotatedPairs(text, ents);
+  return (
+    <>
+      {annotatedPairs.map(function ([text, entLabel], iItem) {
+        let style;
+        if (entLabel === false) {
+          style = STYLE_REGULAR;
+        } else if (
+          [
+            "CARDINAL",
+            "DATE",
+            "MONEY",
+            "ORDINAL",
+            "PERCENT",
+            "QUANTITY",
+            "TIME",
+          ].includes(entLabel)
+        ) {
+          style = STYLE_ENT_NUMBER;
+        } else if (
+          [
+            "FAC",
+            "GPE",
+            "LOC",
+            "NORP",
+            "ORG",
+            "PERSON",
+            "WORK_OF_ART",
+          ].includes(entLabel)
+        ) {
+          style = STYLE_ENT_THING;
+        } else {
+          console.debug(entLabel, text);
+          style = STYLE_REGULAR;
+        }
+
+        return (
+          <span key={"item-" + iItem} style={style}>
+            {text}
+          </span>
+        );
+      })}
+    </>
+  );
+}
