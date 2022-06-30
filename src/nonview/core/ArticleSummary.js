27bad7faf606904c1faeffd903df242cf0c5acb3
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-30 06:57:42 +0530

    Added loadArticleSummaryListForEnt

--- src/nonview/core/ArticleSummary.js
+++ src/nonview/core/ArticleSummary.js
@@ -1,5 +1,6 @@
 import { JSONWWW } from "../../nonview/base/WWW";
 import { URL_DATA } from "../../nonview/constants/Data";
+import Ent from "../../nonview/core/Ent";
 
 const URL_RAW_ARTICLES = URL_DATA + "/articles.summary.latest.json";
 const FILE_NAME_PREFIX = "/tmp/news_lk2/articles/";
@@ -18,6 +19,16 @@ export default class ArticleSummary {
     const rawArticleSummaryList = await jsonWWW.readNoCache();
     return rawArticleSummaryList.map(function (d) {
       return ArticleSummary.fromDict(d);
+    });
+  }
+
+  static async loadArticleSummaryListForEnt(ent) {
+    const entToGroup = await Ent.loadEntToGroup();
+    const group = entToGroup[ent];
+    const groupToArticles = await Ent.loadGroupToArticles();
+    const fileNameList = groupToArticles[group];
+    return fileNameList.map(function (fileName) {
+      return ArticleSummary.fromDict({ file_name: fileName });
     });
   }
 }
